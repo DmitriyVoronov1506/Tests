461c85b7de3fccec7ea6fa801e3417d77ccd0381
DmitriyVoronov1506 <dmitriy.voronov1506@gmail.com>
2023-01-16 11:16:33 +0200

    ClassWork + HomeWorks

--- App/App/Triangle.cs
+++ App/App/Triangle.cs
@@ -11,6 +11,21 @@ namespace App
         public double A { get; set; }
         public double B { get; set; }
         public double C { get; set; }
+        
+        public Triangle()
+        {
+
+        }
+
+        public Triangle(double a, double b, double c)
+        {
+            (A, B, C) = (a, b, c);
+
+            if(!this.IsValid())
+            {
+                throw new ArgumentException($"({A}, {B}, {C}) is not valid triangle");
+            }
+        }
 
         public bool IsValid()
         {
@@ -29,6 +44,32 @@ namespace App
                 Math.Abs(C *C + B*B - A*A) < epsilon * C*C
                 || 
                 Math.Abs(A*A + C*C - B*B) < epsilon * C*C);
+        }
+
+        public double Perimeter()
+        {
+            return A + B + C;
+        }
+
+        public override bool Equals(object? obj)   
+        {  
+            if (obj is Triangle t2)
+            {
+                List<double> t1list = new List<double>() { A, B, C };
+                List<double> t2list = new List<double>() { t2.A, t2.B, t2.C };
+
+                t1list.Sort();
+                t2list.Sort();
+
+                return t1list[0] == t2list[0] && t1list[1] == t2list[1] && t1list[2] == t2list[2];
+            }
+
+            return false;
+        }
+
+        public override int GetHashCode()
+        {
+            return base.GetHashCode();
         }
     }
 }
--- App/UnitTests/TriangleTest.cs
+++ App/UnitTests/TriangleTest.cs
@@ -6,6 +6,49 @@ namespace UnitTests
     public class TriangleTest
     {
         [TestMethod]
+        public void TestDefCtor()
+        {
+            Triangle triangle = new();
+            Assert.IsNotNull(triangle, "Ctor returns Null");
+
+            Triangle triangle2 = new();
+            Assert.AreNotSame(triangle2, triangle, "Ctor returns reference");
+            Assert.AreEqual(triangle2, triangle, "Empty triangles equals");
+        }
+
+        [TestMethod]
+        public void TestParamCtor()
+        {
+            Triangle triangle = new(2, 3, 4);
+            Assert.IsNotNull(triangle, "2-3-4 Ctor is not null");
+            Assert.AreEqual(2, triangle.A, "2-3-4 Ctor -> A = 2");
+            Assert.AreEqual(3, triangle.B, "2-3-4 Ctor -> B = 3");
+            Assert.AreEqual(4, triangle.C, "2-3-4 Ctor -> C = 4");
+
+            Assert.ThrowsException<ArgumentException>(() => triangle = new(1, 2, 3), "new(1, 2, 3) --> Exception");
+
+            var ex = Assert.ThrowsException<ArgumentException>(() => triangle = new(-2, 2, 3), "new(-2, 2, 3) --> Exception");
+
+            Assert.AreEqual("(-2, 2, 3) is not valid triangle", ex.Message, "Exception message invalid");
+
+            Assert.AreEqual(
+                "(3, -2, 3) is not valid triangle",
+                Assert.ThrowsException<ArgumentException>(
+                    () => triangle = new(3, -2, 3),
+                    "new(3, -2, 3) --> Exception").Message,
+                "Exception ,essage invalid");
+
+            Random rand = new();
+
+            for(int i = 0; i < 10; i++)
+            {
+                double c = rand.Next(-100, 0) / 10.0;
+
+                Assert.ThrowsException<ArgumentException>(() => triangle = new(3, 2, c), $"3,2,{c} should be invalid");
+            }
+        }
+
+        [TestMethod]
         public void TestIsValid_1()
         {
             Triangle triangle = new() { A = 1, B = 2, C = 3 };
@@ -85,6 +128,40 @@ namespace UnitTests
 
             triangle = new() { A = 20 * Math.Sqrt(2), B = 20, C = 20 };
             Assert.IsTrue(triangle.IsRight(), $"200-200-200v2 is right {triangle.C * triangle.C}");
+        }
+
+        [TestMethod]
+        public void TestPerimeter()
+        {
+            Triangle triangle = new() { A = 2, B = 2, C = 3 };
+            Assert.AreEqual(7, triangle.Perimeter(), "2-2-3 --> 7");
+
+            triangle = new() { A = 2 * Math.Sqrt(2), B = 2 * Math.Sqrt(2), C = 3 * Math.Sqrt(2) };
+            Assert.AreEqual(7 * Math.Sqrt(2), triangle.Perimeter(), 1e-15 * triangle.Perimeter(), "2-2-3v2 --> 7v2");
+        }
+
+        [TestMethod]
+        public void TestEqual()
+        {
+            Triangle triangle1 = new() { A = 2, B = 3, C = 4 };
+            Triangle triangle2 = new() { A = 4, B = 2, C = 3 };
+
+            Assert.AreEqual(triangle1, triangle2, "Triangles 2-3-4 equals 4-2-3");
+
+            triangle1 = new() { A = 2, B = 3, C = 4 };
+            triangle2 = new() { A = 2, B = 3, C = 4 };
+
+            Assert.AreEqual(triangle1, triangle2, "Triangles 2-3-4 equals 2-3-4");
+
+            triangle1 = new() { A = 4, B = 3, C = 2 };
+            triangle2 = new() { A = 2, B = 3, C = 4 };
+
+            Assert.AreEqual(triangle1, triangle2, "Triangles 4-3-2 equals 2-3-4");
+
+            triangle1 = new() { A = 5, B = 3, C = 2 };
+            triangle2 = new() { A = 2, B = 3, C = 4 };
+
+            Assert.AreNotEqual(triangle1, triangle2, "Triangles 5-3-2 equals 2-3-4");
         }
     }
 }
